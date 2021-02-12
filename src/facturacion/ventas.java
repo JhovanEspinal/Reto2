7254015a658d2ca2ae32776ba69d7bc4d5e2c1ad
Jhovan Espinal <jhovan1028@gmail.com>
2021-02-11 22:00:12 -0500

    Se crea la clase ventas

---
+++ src/facturacion/ventas.java
@@ -0,0 +1,32 @@
+package facturacion;
+import java.util.Scanner;
+public class ventas {
+
+
+    public void menu (){
+     Integer opcion;
+     Scanner lector = new Scanner(System.in);
+
+
+      System.out.println("Bievenido al sistema facturación");
+      System.out.println("Escoga una opción");
+      System.out.println(" 1. Gestionar televisor \n 2. gestionar nevera \n 3.Gestionar electrodomestico \n 4.mostrar ventas \n 5. Salir ");
+
+      opcion = lector.nextInt();
+
+      switch (opcion){
+
+
+
+
+
+
+
+      }
+
+
+
+    }
+
+
+}
