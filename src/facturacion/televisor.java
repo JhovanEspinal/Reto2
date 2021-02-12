e84faa0448beb85e5379e47edbf5cd96dffa686c
Jhovan Espinal <jhovan1028@gmail.com>
2021-02-11 20:49:10 -0500

    Se agrega metodo para calcular precio del televisor

--- src/facturacion/televisor.java
+++ src/facturacion/televisor.java
@@ -5,10 +5,35 @@ public class televisor extends electrodomesticos{
     private boolean TDT;
 
 
-
-
-    public televisor(String nombre, String consumo, String procedencia, int pulgadas) {
+    public televisor(String nombre, Character consumo, String procedencia, int pulgadas) {
         super(nombre, consumo, procedencia);
         this.pulgadas = pulgadas;
+    }
+
+    //calcular precio del televisor
+    @Override
+    public void precio(){
+    Double prec = getPrecio();
+     if (pulgadas > 40){
+         prec+= prec*0.3;
+     }
+
+
+
+    }
+    public int getPulgadas() {
+        return pulgadas;
+    }
+
+    public void setPulgadas(int pulgadas) {
+        this.pulgadas = pulgadas;
+    }
+
+    public boolean isTDT() {
+        return TDT;
+    }
+
+    public void setTDT(boolean TDT) {
+        this.TDT = TDT;
     }
 }
